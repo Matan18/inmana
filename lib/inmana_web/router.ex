aad821cc811ef44df7456087b578fd907a029e0f
Mateus Andriola <mateusandriola18@gmail.com>
2021-04-23 22:42:55 -0300

    Implement routes

--- lib/inmana_web/router.ex
+++ lib/inmana_web/router.ex
@@ -11,6 +11,8 @@ defmodule InmanaWeb.Router do
     get "/", WellcomeController, :index
 
     post "/restaurants", RestaurantsController, :create
+
+    resources "/supplies", SuppliesController, only: [:create, :show]
   end
 
   # Enables LiveDashboard only for development
@@ -27,5 +29,9 @@ defmodule InmanaWeb.Router do
       pipe_through [:fetch_session, :protect_from_forgery]
       live_dashboard "/dashboard", metrics: InmanaWeb.Telemetry
     end
+  end
+
+  if Mix.env() in [:dev] do
+    forward "/sent_mails", Bamboo.SentEmailViewerPlug
   end
 end
